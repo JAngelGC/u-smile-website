d74dc1f704eaa92b49dfc9ee533d89fb27b209fb
JAngelGC <ja_gonzalez42@hotmail.com>
2023-04-08 00:06:35 -0600

    Adding section info ui component

---
+++ src/components/ui/SectionInfo.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import classes from "./SectionInfo.module.css";
+
+const SectionInfo: React.FC<{
+  textSide: string;
+  title: string;
+  text: string;
+}> = (props) => {
+  const classOrientation: string =
+    props.textSide === "left" ? "leftText" : "rightSide";
+
+  return (
+    <section
+      className={`${classes["section-info"]} ${classes[classOrientation]}`}
+    >
+      <div className={classes["box-img"]}>
+        <Image
+          src={"/img/Kid-smiling.png"}
+          fill
+          style={{ objectFit: "cover" }}
+          alt=""
+        />
+      </div>
+
+      <div className={classes["box-text"]}>
+        <h2>{props.title}</h2>
+        <p>{props.text}</p>
+      </div>
+    </section>
+  );
+};
+
+export default SectionInfo;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Header from "@/components/homePage/Header";
+import SectionInfo from "@/components/ui/SectionInfo";
 
 export default function Home() {
   return (
@@ -13,6 +14,21 @@ export default function Home() {
       </Head>
       <section>
         <Header />
+        <SectionInfo
+          textSide="left"
+          title="Sonríe con confianza"
+          text="En USmile nos especializamos en ofrecer tratamientos de ortodoncia modernos y de alta calidad a través de alineadores invisibles en un ambiente amigable y acogedor."
+        />
+        <SectionInfo
+          textSide="right"
+          title="Nuestro equipo"
+          text="Contamos con dentistas altamente capacitados y experimentados están comprometidos con la excelencia en el cuidado dental y la atención al paciente."
+        />
+        <SectionInfo
+          textSide="left"
+          title="Cada paciente"
+          text="Trabajamos juntos para desarrollar un plan de tratamiento personalizado que satisfaga sus objetivos de tu salud bucal"
+        />
       </section>
     </>
   );
