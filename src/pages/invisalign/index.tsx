7f4663664d3236b95e833d92e5e33df266a25f28
JAngelGC <ja_gonzalez42@hotmail.com>
2023-04-09 17:58:37 -0600

    Adding introduction for the page

--- src/pages/invisalign/index.tsx
+++ src/pages/invisalign/index.tsx
@@ -1,5 +1,16 @@
+import SectionTitle from "@/components/ui/SectionTitle";
+import SectionInfoLarge from "@/components/ui/SectionInfoLarge";
+
 const InvisalignPage = () => {
-  return <></>;
+  return (
+    <>
+      <SectionTitle title="Invisalign" />
+      <SectionInfoLarge
+        title="La mejor tecnología en Puebla"
+        text="Uno de los tratamientos más populares en nuestra clínica es el sistema Invisalign, que ofrece una alternativa discreta y cómoda a los brackets y alambres tradicionales."
+      />
+    </>
+  );
 };
 
 export default InvisalignPage;
