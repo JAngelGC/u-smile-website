fe4e4b3412d6243582193fc183b5c332e5e02585
JAngelGC <ja_gonzalez42@hotmail.com>
2023-04-09 17:32:05 -0600

    Adding treatments intro component

--- src/pages/tratamientos/index.tsx
+++ src/pages/tratamientos/index.tsx
@@ -1,10 +1,12 @@
 import SectionTitle from "@/components/ui/SectionTitle";
+import TreatmentsIntro from "@/components/treatmentsPage/TreatmentsIntro";
 import TreatmentsAccordion from "@/components/treatmentsPage/TreatmentsAccordion";
 
 const TreatmentsPage = () => {
   return (
     <>
       <SectionTitle title="Tratamientos" />
+      <TreatmentsIntro />
       <TreatmentsAccordion />
     </>
   );
