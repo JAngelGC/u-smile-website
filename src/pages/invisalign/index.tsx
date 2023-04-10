5d9a5d47c65b0a9ba795c63ca3b6befcf6b8d2f8
JAngelGC <ja_gonzalez42@hotmail.com>
2023-04-09 18:01:29 -0600

    Adding all section info

--- src/pages/invisalign/index.tsx
+++ src/pages/invisalign/index.tsx
@@ -1,5 +1,6 @@
 import SectionTitle from "@/components/ui/SectionTitle";
 import SectionInfoLarge from "@/components/ui/SectionInfoLarge";
+import SectionInfo from "@/components/ui/SectionInfo";
 
 const InvisalignPage = () => {
   return (
@@ -8,6 +9,21 @@ const InvisalignPage = () => {
       <SectionInfoLarge
         title="La mejor tecnología en Puebla"
         text="Uno de los tratamientos más populares en nuestra clínica es el sistema Invisalign, que ofrece una alternativa discreta y cómoda a los brackets y alambres tradicionales."
+      />
+      <SectionInfo
+        textSide="right"
+        title="Personalización"
+        text="Nuestro equipo de ortodoncistas altamente capacitados y experimentados trabaja en estrecha colaboración con cada paciente para diseñar un plan de tratamiento personalizado que aborde sus necesidades únicas."
+      />
+      <SectionInfo
+        textSide="left"
+        title="Comodidad"
+        text="En U Smile, creemos que la comodidad del paciente es una parte integral de cualquier tratamiento de ortodoncia exitoso. Los alineadores Invisalign son removibles, lo que permite una mayor comodidad y una higiene bucal más fácil durante el tratamiento"
+      />
+      <SectionInfo
+        textSide="right"
+        title="Apariencia"
+        text="Debido a que los alineadores son transparentes, los pacientes pueden continuar con su vida diaria sin tener que preocuparse por la apariencia de sus brackets y alambres."
       />
     </>
   );
