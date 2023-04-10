e1ee443d97dbf2a8b9d24f1aa27aa8032d6db4cc
JAngelGC <ja_gonzalez42@hotmail.com>
2023-04-09 20:11:37 -0600

    Adding map section to the contact page

--- src/pages/contacto/index.tsx
+++ src/pages/contacto/index.tsx
@@ -1,5 +1,15 @@
+import SectionTitle from "@/components/ui/SectionTitle";
+import ContactInfo from "@/components/contactPage/ContactInfo";
+import LocationMap from "@/components/contactPage/LocationMap";
+
 const ContactPage = () => {
-  return <></>;
+  return (
+    <>
+      <SectionTitle title="Contacto" />
+      <ContactInfo />
+      <LocationMap />
+    </>
+  );
 };
 
 export default ContactPage;
