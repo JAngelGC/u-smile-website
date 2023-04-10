684d18eaf0018ea97a92b4534e14daabc2327f8c
JAngelGC <ja_gonzalez42@hotmail.com>
2023-04-09 20:13:20 -0600

    Adding cta to contact page

--- src/pages/contacto/index.tsx
+++ src/pages/contacto/index.tsx
@@ -1,6 +1,7 @@
 import SectionTitle from "@/components/ui/SectionTitle";
 import ContactInfo from "@/components/contactPage/ContactInfo";
 import LocationMap from "@/components/contactPage/LocationMap";
+import CallToAction from "@/components/ui/CallToAction";
 
 const ContactPage = () => {
   return (
@@ -8,6 +9,10 @@ const ContactPage = () => {
       <SectionTitle title="Contacto" />
       <ContactInfo />
       <LocationMap />
+      <CallToAction
+        bgColor="white"
+        text="¡Estamos ansiosos de verte en nuestra clínica!"
+      />
     </>
   );
 };
