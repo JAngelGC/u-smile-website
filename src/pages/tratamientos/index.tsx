9bed9e146088d70c7cc29cf938189445becb5b43
JAngelGC <ja_gonzalez42@hotmail.com>
2023-04-16 00:20:41 -0600

    Treatmentpage styles updated

--- src/pages/tratamientos/index.tsx
+++ src/pages/tratamientos/index.tsx
@@ -12,6 +12,7 @@ const TreatmentsPage = () => {
       <CallToAction
         bgColor="green"
         text="Estamos ansiosos por ayudarte a lograr una sonrisa saludable y hermosa"
+        imagePath="/img/Usmile-fotografia-7.jpg"
       />
     </>
   );
