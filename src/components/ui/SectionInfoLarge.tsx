5e1ac0c9cc873e0be835a7b5f98fc0ffd86b680d
JAngelGC <ja_gonzalez42@hotmail.com>
2023-04-15 23:56:33 -0600

    Aboutpage styles updated

--- src/components/ui/SectionInfoLarge.tsx
+++ src/components/ui/SectionInfoLarge.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import classes from "./SectionInfoLarge.module.css";
 
-const SectionInfoLarge: React.FC<{ title?: string; text: string }> = (
-  props
-) => {
+const SectionInfoLarge: React.FC<{
+  title?: string;
+  text: string;
+  imagePath: string;
+}> = (props) => {
   return (
     <section className={classes["section-info"]}>
       <h2>{props.title}</h2>
       <div className={classes["box-img"]}>
         <Image
-          src={"/img/Kid-smiling.png"}
+          src={props.imagePath}
           fill
           style={{ objectFit: "cover" }}
           alt=""
--- src/pages/quienes-somos/index.tsx
+++ src/pages/quienes-somos/index.tsx
@@ -8,12 +8,16 @@ const aboutPage = () => {
   return (
     <>
       <SectionTitle title="Quiénes somos" />
-      <SectionInfoLarge text="Somos un equipo de profesionales que tienen por objetivo, brindarte los mejores servicios odontológicos de la más alta calidad, materiales, ubicación e instalaciones de vanguardia." />
+      <SectionInfoLarge
+        text="Somos un equipo de profesionales que tienen por objetivo, brindarte los mejores servicios odontológicos de la más alta calidad, materiales, ubicación e instalaciones de vanguardia."
+        imagePath="/img/Usmile-fotografia-5.jpg"
+      />
       <SectionAdvantages />
       <SectionLogo text="En U Smile, tenemos un compromiso contigo, ofrecerte un tratamiento con el que te sientas cómodo y diseñado perfectamente a la medida para ti" />
       <CallToAction
         bgColor="white"
         text="Descubre cómo podemos ayudarte a lograr una sonrisa saludable y hermosa"
+        imagePath="/img/Usmile-fotografia-6.jpg"
       />
     </>
   );
